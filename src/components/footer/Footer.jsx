0accf4c1254355d4af90387bb0930bfe53ad411b
Souma <mondal.souma@gmail.com>
2023-11-16 12:03:41 -0800

    adds footer and improvements to responsiveness

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,79 @@
 import React from 'react';
 import styles from './footer.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Footer = () => {
-    return <div className={styles.container}>Footer</div>;
+    return (
+        <div className={styles.container}>
+            <div className={styles.info}>
+                <div className={styles.logo}>
+                    <Image
+                        src="/logo.png"
+                        alt="suma blog"
+                        width={50}
+                        height={50}
+                    />
+                    <h1 className={styles.logoText}>Suma Blog</h1>
+                </div>
+                <p className={styles.desc}>
+                    Nam mi eget erat hendrerit vulputate. Praesent sit amet
+                    euismod urna, et luctus velit. Aliquam blandit tellus in
+                    erat malesuada pharetra. Aenean eu cursus libero. Vivamus
+                    euismod tristique felis, eu commodo nisl mollis at.
+                </p>
+                <div className={styles.icons}>
+                    <Image
+                        src="/facebook.png"
+                        alt="facebook"
+                        width={18}
+                        height={18}
+                    />
+                    <Image
+                        src="/instagram.png"
+                        alt="instagram"
+                        width={18}
+                        height={18}
+                    />
+                    <Image
+                        src="/tiktok.png"
+                        alt="tiktok"
+                        width={18}
+                        height={18}
+                    />
+                    <Image
+                        src="/youtube.png"
+                        alt="youtube"
+                        width={18}
+                        height={18}
+                    />
+                </div>
+            </div>
+            <div className={styles.links}>
+                <div className={styles.list}>
+                    <span className={styles.listTitle}>Links</span>
+                    <Link href="/">Homepage</Link>
+                    <Link href="/">Blog</Link>
+                    <Link href="/">About</Link>
+                    <Link href="/">Contact</Link>
+                </div>
+                <div className={styles.list}>
+                    <span className={styles.listTitle}>Tags</span>
+                    <Link href="/">Style</Link>
+                    <Link href="/">Coding</Link>
+                    <Link href="/">Travel</Link>
+                    <Link href="/">Food</Link>
+                </div>
+                <div className={styles.list}>
+                    <span className={styles.listTitle}>Social </span>
+                    <Link href="/">Facebook</Link>
+                    <Link href="/">Tiktok</Link>
+                    <Link href="/">Instagram</Link>
+                    <Link href="/">Youtube</Link>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
