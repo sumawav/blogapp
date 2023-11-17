7749b9fca7106627ad390374e7f15436afb17aae
Souma <mondal.souma@gmail.com>
2023-11-17 12:09:28 -0800

    adds category page and basic single page

---
+++ src/app/[slug]/page.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './singlePage.module.css';
+
+function SinglePage() {
+    return <div className={styles.container}></div>;
+}
+
+export default SinglePage;
---
+++ src/app/blog/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './blogPage.module.css';
+import Cardlist from '@/components/cardlist/Cardlist';
+import Menu from '@/components/menu/Menu';
+
+function BlogPage() {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>style blog</h1>
+            <div className={styles.content}>
+                <Cardlist />
+                <Menu />
+            </div>
+        </div>
+    );
+}
+
+export default BlogPage;
