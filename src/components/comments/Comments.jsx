c474edfe3b1559dcb8be8ccdc7a4d5de6a50fde7
Souma <mondal.souma@gmail.com>
2023-11-21 12:12:44 -0800

    makes singlepage view responsive. fixes css typos

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -27,14 +27,74 @@ function Comments() {
                             alt=""
                             width={50}
                             height={50}
-                            className={styles.Image}
+                            className={styles.image}
                         />
                         <div className={styles.userInfo}>
                             <span className={styles.username}>Souma</span>
                             <span className={styles.date}>01.22.2024</span>
                         </div>
                     </div>
-                    <p className={styles.desc}>
+                    <p className={styles.description}>
+                        Suspendisse vitae eros dui. Donec rhoncus vehicula
+                        porttitor. Nam scelerisque enim sollicitudin purus
+                        porttitor ullamcorper. Sed sit amet dui dolor.
+                    </p>
+                </div>
+                <div className={styles.comment}>
+                    <div className={styles.user}>
+                        <Image
+                            src="/p1.jpeg"
+                            alt=""
+                            width={50}
+                            height={50}
+                            className={styles.image}
+                        />
+                        <div className={styles.userInfo}>
+                            <span className={styles.username}>Souma</span>
+                            <span className={styles.date}>01.22.2024</span>
+                        </div>
+                    </div>
+                    <p className={styles.description}>
+                        Suspendisse vitae eros dui. Donec rhoncus vehicula
+                        porttitor. Nam scelerisque enim sollicitudin purus
+                        porttitor ullamcorper. Sed sit amet dui dolor.
+                    </p>
+                </div>
+                <div className={styles.comment}>
+                    <div className={styles.user}>
+                        <Image
+                            src="/p1.jpeg"
+                            alt=""
+                            width={50}
+                            height={50}
+                            className={styles.image}
+                        />
+                        <div className={styles.userInfo}>
+                            <span className={styles.username}>Souma</span>
+                            <span className={styles.date}>01.22.2024</span>
+                        </div>
+                    </div>
+                    <p className={styles.description}>
+                        Suspendisse vitae eros dui. Donec rhoncus vehicula
+                        porttitor. Nam scelerisque enim sollicitudin purus
+                        porttitor ullamcorper. Sed sit amet dui dolor.
+                    </p>
+                </div>
+                <div className={styles.comment}>
+                    <div className={styles.user}>
+                        <Image
+                            src="/p1.jpeg"
+                            alt=""
+                            width={50}
+                            height={50}
+                            className={styles.image}
+                        />
+                        <div className={styles.userInfo}>
+                            <span className={styles.username}>Souma</span>
+                            <span className={styles.date}>01.22.2024</span>
+                        </div>
+                    </div>
+                    <p className={styles.description}>
                         Suspendisse vitae eros dui. Donec rhoncus vehicula
                         porttitor. Nam scelerisque enim sollicitudin purus
                         porttitor ullamcorper. Sed sit amet dui dolor.
