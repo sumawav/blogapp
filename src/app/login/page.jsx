74590a28ec9885e6d7377f492af7ece664c16702
Souma <mondal.souma@gmail.com>
2023-11-27 05:00:13 -0800

    adds login page

---
+++ src/app/login/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './loginPage.module.css';
+
+function LoginPage() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <div className={styles.socialButton}>Sign in with Github</div>
+                <div className={styles.socialButton}>Sign in with Github</div>
+                <div className={styles.socialButton}>Sign in with Github</div>
+            </div>
+        </div>
+    );
+}
+
+export default LoginPage;
