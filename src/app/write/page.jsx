e4f217ffa26af5db0a05e646b3fc4154dfc33deb
Souma <mondal.souma@gmail.com>
2024-02-12 12:43:38 -0800

    adds publish button

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -11,7 +11,7 @@ const WritePage = () => {
 
     return (
         <div className={styles.container}>
-            <input type="text" placeholder="Title" />
+            <input type="text" placeholder="Title" className={styles.input} />
             <div className={styles.editor}>
                 <button
                     className={styles.button}
@@ -48,12 +48,14 @@ const WritePage = () => {
                     </div>
                 )}
                 <ReactQuill
+                    className={styles.textArea}
                     theme="bubble"
                     value={value}
                     onChange={setValue}
                     placeholder="Talk to me..."
                 />
             </div>
+            <button className={styles.publish}>Publish</button>
         </div>
     );
 };
